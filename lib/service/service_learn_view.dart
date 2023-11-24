d3b8362ae0713ec62fdf5f7eec1d9d7457e5b06a
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-24 06:54:44 +0300

    get işlemi bitti

--- lib/service/service_learn_view.dart
+++ lib/service/service_learn_view.dart
@@ -14,11 +14,24 @@ class ServiceLearn extends StatefulWidget {
 class _ServiceLearnState extends State<ServiceLearn> {
   List<PostModel>? _items;
   bool _isLoading = false;
+  late final Dio _dio;
+  final _baseUrl='https://jsonplaceholder.typicode.com/';
+
+
+
   void changeLoading() {
     setState(() {
       _isLoading = !_isLoading;
     });
   }
+
+   @override
+  void initState() {
+    super.initState();
+    _dio=Dio(BaseOptions(baseUrl: _baseUrl));
+    verileriGetir();
+  }
+
 
   Future<void> verileriGetir() async {
     changeLoading();// Veri var mı yok mu kontrolü
@@ -35,12 +48,22 @@ class _ServiceLearnState extends State<ServiceLearn> {
     changeLoading();
   }
 
-  @override
-  void initState() {
-    super.initState();
-    verileriGetir();
+  Future<void> verileriGetirAdvanced() async {
+    changeLoading();// Veri var mı yok mu kontrolü
+    final response =await _dio.get('posts');
+    if (response.statusCode == HttpStatus.ok) {
+      final _datas = response.data;
+      if (_datas is List) {
+        setState(() {
+          _items = _datas.map((e) => PostModel.fromJson(e)).toList();
+        });
+      }
+    }
+    changeLoading();
   }
 
+
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,19 +72,35 @@ class _ServiceLearnState extends State<ServiceLearn> {
         actions: [_isLoading ? CircularProgressIndicator() : SizedBox.shrink()],
       ),
       body: ListView.builder(
+        padding: EdgeInsets.all(8),
           itemCount: _items?.length ?? 0,
           itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(_items?[index].title ?? '',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red)),
-                subtitle: Text(_items?[index].body ?? ''),
-              ),
-            );
+            return _PostCard(model: _items?[index]);
           }),
+    );
+  }
+}
+
+class _PostCard extends StatelessWidget {
+  const _PostCard({
+    super.key,
+    required PostModel? model,
+  }) : _model = model;
+
+  final PostModel? _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: ListTile(
+        title: Text(_model?.title ?? '',
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.red)),
+        subtitle: Text(_model?.body ?? ''),
+      ),
     );
   }
 }
